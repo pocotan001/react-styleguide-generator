910e44fc21c8d02db87cf95935feb4159d8b5f58
pocotan001 <pocotan001@gmail.com>
2015-06-26 13:08:51 +0900

    :white_check_mark:

--- test/rsg.js
+++ test/rsg.js
@@ -1,4 +1,4 @@
-/* global describe, it, afterEach */
+/* global describe, it, after */
 
 var assert = require('assert')
 var path = require('path')
@@ -32,8 +32,11 @@ function getRealPath (file) {
 
 describe('RSG', function () {
 
-  describe('input', function () {
+  after(function () {
+    fs.removeSync(TMP_DIR)
+  })
 
+  describe('input', function () {
     it('should throw an exception when the value is not present', function () {
       assert.throws(RSG.bind(), Error)
     })
@@ -73,33 +76,104 @@ describe('RSG', function () {
     })
   })
 
-  describe('#generate', function () {
-    this.timeout(0)
-
-    afterEach(function () {
-      fs.removeSync(TMP_DIR)
+  describe('opts.root', function () {
+    it('should default to null', function () {
+      var rsg = RSG(INPUT_FILE, { root: undefined })
+      assert.equal(rsg.opts.root, null)
     })
 
-    // it('files should exist', function (done) {
-    //   RSG(INPUT_FILE).generate(function (err) {
-    //     assert.ifError(err)
+    it('should be a "foo"', function () {
+      var rsg = RSG(INPUT_FILE, { root: 'foo' })
+      assert.equal(rsg.opts.root, '/foo')
+    })
+  })
 
-    //     var files = [
-    //       'index.html',
-    //       'src/app.css',
-    //       'src/app.js',
-    //       'src/contents.js',
-    //       'src/react_' + this.reactVersion + '.js'
-    //     ].map(function (file) { return process.cwd() + '/' + file })
+  describe('opts.pushstate', function () {
+    it('should default to false', function () {
+      var rsg = RSG(INPUT_FILE, { pushstate: undefined })
+      assert.equal(rsg.opts.pushstate, false)
+    })
 
-    //     assert(false)
-    //     assert(files.every(function (file) { return fs.existsSync(file) }))
-    //     done()
-    //   })
-    // })
+    it('should be a true', function () {
+      var rsg = RSG(INPUT_FILE, { pushstate: true })
+      assert.equal(rsg.opts.pushstate, true)
+    })
+  })
 
-    it('should invoke the callback', function (done) {
-      RSG(INPUT_FILE).generate(done)
+  describe('opts.files', function () {
+    it('should default to []', function () {
+      var rsg = RSG(INPUT_FILE, { files: undefined })
+      assert.deepEqual(rsg.opts.files, [])
+    })
+
+    it('should be the files', function () {
+      var files = ['a.css', 'a.js']
+      var rsg = RSG(INPUT_FILE, { files: files })
+      assert.deepEqual(rsg.opts.files, files)
+    })
+  })
+
+  describe('opts.babelConfig', function () {
+    it('should default to null', function () {
+      var rsg = RSG(INPUT_FILE, { babelConfig: undefined })
+      assert.deepEqual(rsg.opts.babelConfig, null)
+    })
+
+    it('should be an object', function () {
+      var babelConfig = { stage: 0 }
+      var rsg = RSG(INPUT_FILE, { babelConfig: babelConfig })
+      assert.deepEqual(rsg.opts.babelConfig, babelConfig)
+    })
+  })
+
+  describe('opts.config', function () {
+    it('should load styleguide.json', function () {
+      var rsg = RSG(INPUT_FILE, { config: undefined })
+      assert.equal(rsg.opts.title, 'React Style Guide')
+    })
+
+    it('should be overwritten', function () {
+      var rsg = RSG(INPUT_FILE, { config: undefined, title: 'foo' })
+      assert.equal(rsg.opts.title, 'foo')
+    })
+
+    it('should custom config file', function () {
+      var src = TMP_DIR + '/foo.json'
+      fs.outputFileSync(src, '{ "title": "foo" }')
+
+      var rsg = RSG(INPUT_FILE, { config: src })
+      assert.equal(rsg.opts.title, 'foo')
+      fs.removeSync(src)
+    })
+
+  })
+
+  describe('#generate', function () {
+    // this test is too heavy...
+    this.timeout(60000)
+
+    var opts = {
+      files: [
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
+        'example/example.css'
+      ]
+    }
+
+    var rsg = RSG(INPUT_FILE, opts)
+    var generated = rsg.generate()
+
+    it('files should exist', function (done) {
+      var files = [
+        'index.html',
+        'src/contents.js',
+        'src/react_' + rsg.reactVersion + '.js',
+        'files/example.css'
+      ].map(function (file) { return getRealPath(TMP_DIR + '/' + file) })
+
+      generated.then(function () {
+        assert(files.every(function (file) { return fs.existsSync(file) }))
+        done()
+      }).catch(done)
     })
   })
 
